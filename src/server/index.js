70d2c9330d0f2e68d6429c40e933a54b91b51fc7
Shaik Sadath Basha <sadath.sa@gmail.com>
2020-11-26 17:29:32 +0530

    Number Of Matches Won Per Year Function Implemented

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+const fs = require('fs');
+const csv = require('csvtojson');
+const path = require('path');
+
+const matchesPerYear = require('./ipl_stats/matchesPerYear');
+
+const csvFilePath = path.join(__dirname, '../data');
+const outputPath = path.join(__dirname, '../public/output');
+
+const writeFile = (outputFilename, obj) => {
+  fs.writeFile(`${outputPath}/${outputFilename}`, JSON.stringify(obj), (err) => {
+    if (err) {
+      console.error(`Error Occurred processing ${outputPath}/${outputFilename}`);
+    } else {
+      console.log(`${outputFilename} file Saved Successfully`);
+    }
+  });
+};
+
+csv()
+  .fromFile(`${csvFilePath}/matches.csv`)
+  .then((matchesArray) => {
+    csv()
+      .fromFile(`${csvFilePath}/deliveries.csv`)
+      .then((deliveriesArray) => {
+          writeFile('matchesPerYear.json', matchesPerYear(matchesArray));
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(Error('Error Occurred While Processing Deliveries csv file'));
+        }
+      });
+  })
+  .catch((err) => {
+    if (err) {
+      console.error(Error('Error Occurred While Processing Matches csv file'));
+    }
+  });
+
+
---
+++ src/server/ipl_stats/matchesPerYear.js
@@ -0,0 +1,14 @@
+const matchesPerYear = (matchesArray) => {
+  const matchesPerYearObject = matchesArray.reduce((acc, obj) => {
+    if (acc[obj.season] === undefined) {
+      acc[obj.season] = 1;
+    } else {
+      acc[obj.season] += 1;
+    }
+    return acc;
+  }, {});
+
+  return matchesPerYearObject;
+};
+
+module.exports = matchesPerYear;
