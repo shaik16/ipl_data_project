5bdb78e2fe86cfef7172f37b72337c0ad61ebdda
Shaik Sadath Basha <sadath.sa@gmail.com>
2020-12-07 21:14:00 +0530

    1.Deleted Old routes.js file which used http module. 2.Implemented the routes using express in the index.js. 3.Created separate router queryApi.js for serving api request.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,17 @@
-const http = require('http');
+const express = require('express');
 const path = require('path');
-const routes = require('./routes');
 const iplStats = require('./ipl');
 const env = require('./config');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/api', require('./routes/api/queryApi'));
+
+app.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '../public', '404.html'));
+});
 
 const csvFilePath = path.join(__dirname, '../data');
 
@@ -10,9 +19,9 @@ const loadData = async () => {
   try {
     await iplStats(csvFilePath);
 
-    const server = http.createServer(routes);
-
-    server.listen(env.port, () => console.log(`server started on port ${server.address().port}`));
+    app.listen(env.port, () => {
+      console.log(`Server started on ${env.port}`);
+    });
   } catch (err) {
     console.log('Data loading Unsuccessful');
     console.log(err);
---
+++ src/server/routes/api/queryApi.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const dbConnection = require('../../databaseConnection');
+const matchesPerYear = require('../../ipl_stats/matchesPerYear');
+const teamWonMatchesPerYear = require('../../ipl_stats/teamWonMatchesPerYear');
+const extraRunsConcededPerTeam = require('../../ipl_stats/extraRunsConcededPerTeam');
+const topTenEconomicBowlers = require('../../ipl_stats/topTenEconomicBowlers');
+
+const route = express.Router();
+
+route.get('/', (req, res) => {
+  res.status(400).json({
+    message:
+      'append /api url  with matchesPerYear, teamWonPerYear, extraRunsPerTeam, and topTenEconomicBowlers',
+  });
+});
+
+const databaseSession = async (res, queryCallBack, year) => {
+  try {
+    const connection = await dbConnection.connect();
+    if (year === undefined) {
+      const result = await queryCallBack(connection);
+      res.json(result);
+    } else {
+      const result = await queryCallBack(connection, year);
+      res.json(result);
+    }
+    connection.release();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+route.get('/matchesPerYear', (req, res) => {
+  databaseSession(res, matchesPerYear);
+});
+
+route.get('/teamWonPerYear', async (req, res) => {
+  databaseSession(res, teamWonMatchesPerYear);
+});
+
+route.get('/extraRunsPerTeam', async (req, res) => {
+  databaseSession(res, extraRunsConcededPerTeam, 2016);
+});
+
+route.get('/topTenEconomicBowlers', async (req, res) => {
+  databaseSession(res, topTenEconomicBowlers, 2015);
+});
+
+route.get('/*', (req, res) => {
+  res.status(400).json({
+    message: 'Append api with proper string matchesPerYear or teamWonPerYear etc',
+  });
+});
+
+module.exports = route;
